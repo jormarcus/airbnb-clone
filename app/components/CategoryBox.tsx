02aa025c85f526ff803882936934478d3e52d724
jormarcus <jormarcus92@gmail.com>
2023-04-17 15:08:14 -0400

    Add Categories

---
+++ app/components/CategoryBox.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import qs from 'query-string';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
+import { IconType } from 'react-icons';
+
+interface CategoryBoxProps {
+  icon: IconType;
+  label: string;
+  selected?: boolean;
+}
+
+const CategoryBox: React.FC<CategoryBoxProps> = ({
+  icon: Icon,
+  label,
+  selected,
+}) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      // convert the params to an object
+      currentQuery = qs.parse(params.toString());
+    }
+
+    // update the query to include the category
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    // if the category we clicked on is already selected
+    // remove it from the query, to deselect it
+    if (params?.get('category') === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: '/',
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, router, params]);
+
+  return (
+    <div
+      onClick={handleClick}
+      className={`
+        flex 
+        cursor-pointer 
+        flex-col 
+        items-center 
+        justify-center
+        gap-2
+        border-b-2
+        p-3
+        transition
+        hover:text-neutral-800
+        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+        ${selected ? 'text-neutral-800' : 'text-neutral-500'}
+      `}
+    >
+      <Icon size={26} />
+      <div className="text-sm font-medium">{label}</div>
+    </div>
+  );
+};
+
+export default CategoryBox;
