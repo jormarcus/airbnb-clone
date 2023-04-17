edf639f4b4f2afcb45338473e784ed4e7a297d1d
jormarcus <jormarcus92@gmail.com>
2023-04-17 15:10:33 -0400

    Go home logo click

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,14 +1,15 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 const Logo = () => {
   const router = useRouter();
   return (
     <Image
+      onClick={() => router.push('/')}
       alt="Logo"
-      className="hidden md:block cursor-pointer"
+      className="hidden cursor-pointer md:block"
       height="100"
       width="100"
       src="/images/logo.png"
