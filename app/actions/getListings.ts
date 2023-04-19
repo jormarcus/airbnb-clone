db5163f020b4da61922fa0d0203bdee6a5b2ac40
jormarcus <jormarcus92@gmail.com>
2023-04-19 15:12:26 -0400

    ListingCard and favorite

---
+++ app/actions/getListings.ts
@@ -0,0 +1,21 @@
+import prisma from '@/app/libs/prismadb';
+import { SafeListing } from '../types';
+
+export default async function getListings() {
+  try {
+    const listings = await prisma.listing.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    const safeListings: SafeListing[] = listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+    }));
+
+    return safeListings;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import { getCurrentUser } from './actions/getCurrentUser';
+import getListings from './actions/getListings';
 import ClientOnly from './components/ClientOnly';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
+import ListingCard from './components/listings/ListingCard';
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
@@ -17,7 +21,13 @@ export default function Home() {
     <ClientOnly>
       <Container>
         <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-          <div>My future listings</div>
+          {listings.map((listing) => (
+            <ListingCard
+              key={listing.id}
+              data={listing}
+              currentUser={currentUser}
+            ></ListingCard>
+          ))}
         </div>
       </Container>
     </ClientOnly>
